58a0399b1763d914bbfcea901ae71bf837132f3d
Marcos Silva <msilva@hopefulinc.me>
2021-09-28 10:14:23 -0400

    feat: add App.js and app UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import {
+	Container,
+	Grid,
+	Header,
+	Statistic,
+	Segment,
+	Icon,
+	Form,
+	Button
+} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Header as='h1'>Budget App</Header>
+			<Statistic size='small'>
+				<Statistic.Label>Your Balance:</Statistic.Label>
+				<Statistic.Value>2,550.35</Statistic.Value>
+			</Statistic>
+			<Segment textAlign='center'>
+				<Grid columns={2} divided>
+					<Grid.Row>
+						<Grid.Column>
+							<Statistic size='tiny' color='green'>
+								<Statistic.Label style={{ textAlign: 'left' }}>
+									Income:
+								</Statistic.Label>
+								<Statistic.Value>1,045.50</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+						<Grid.Column>
+							<Statistic size='tiny' color='red'>
+								<Statistic.Label style={{ textAlign: 'left' }}>
+									Expenses:
+								</Statistic.Label>
+								<Statistic.Value>525.50</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
+			<Header as='h3'>History</Header>
+			<Segment color='red'>
+				<Grid columns={3} textAlign='right'>
+					<Grid.Row>
+						<Grid.Column width={10} textAlign='left'>
+							Something
+						</Grid.Column>
+						<Grid.Column width={3} textAlign='right'>
+							$10,00
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Icon name='edit' boardered />
+							<Icon name='trash' />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
+			<Header as='h3'>Add new transaction</Header>
+			<Form unstackable>
+				<Form.Group>
+					<Form.Input
+						label='Description'
+						icon='tags'
+						width={12}
+						placeholder='New shinny thing'
+					/>
+					<Form.Input
+						label='value'
+						width={4}
+						placeholder='100.00'
+						icon='dollar'
+						iconPosition='left'
+					/>
+				</Form.Group>
+				<Button.Group style={{ marginTop: 20 }}>
+					<Button style={{ width: 100 }}>Cancel</Button>
+					<Button.Or />
+					<Button primary style={{ width: 100 }}>
+						Ok
+					</Button>
+				</Button.Group>
+			</Form>
+		</Container>
+	);
 }
 
 export default App;
